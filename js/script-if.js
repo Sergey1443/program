7fd80ec1e4f780de667cf33b2b0fa2863412ce1b
Sergey1443 <serik110709@gmail.com>
2025-04-18 09:52:04 +0500

    task2

--- js/script-if.js
+++ js/script-if.js
@@ -84,14 +84,35 @@ while (x !== 0) {
 alert("Минимальная цифра: " + min);
 
 }
+
 {
     /* 5.53Известны оценки ученика по 10 предметам. Определить среднюю оценку. x- оценка y- средняя*/
 
-    let sum =0;
-    for (let i=0; i <= 10; i++){
-        let x = parseInt(prompt("введите оценку" ));
+    let sum = 0;
+    for (let i=1; i <= 10; i++){
+        let x = parseInt(prompt("введите оценку"+ i +":" ));
         sum += x;
     } 
     let y = sum / 10;
     alert("Средняя оценка: " + y); 
 }
+
+{/* 4.29 Дано трехзначное число. Определить, равен ли квадрат этого числа сумме ку-
+бов его цифр. */
+
+
+    let number = parseInt(prompt("Введите трехзначное число:"));
+    const x = number % 10;
+    const y = Math.floor(number / 10) % 10
+    const z = Math.floor(number / 100) % 10 
+    
+    const sqrtOfNumber = number * number;
+    const sumOfCubes = Math.pow(x, 3)+ Math.pow(y, 3) +  Math.pow(z, 3);
+    if (sqrtOfNumber ==sumOfCubes) {
+        alert("Квадрат числа  равен сумме кубов его цифр"+ number);
+    } else {
+        alert("Квадрат числа  НЕ равен сумме кубов его цифр"+ number);
+    }
+}
+
+
